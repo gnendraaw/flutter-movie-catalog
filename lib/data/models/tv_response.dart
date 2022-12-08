590ca2bbdcccd24e00c229b7164e81fb750b54f1
gnendraaw <itsgnendraaw@gmail.com>
2022-12-08 17:44:24 +0800

    update mocking test file

--- lib/data/models/tv_response.dart
+++ lib/data/models/tv_response.dart
@@ -7,10 +7,9 @@ class TvResponse extends Equatable {
   TvResponse({required this.tvList});
 
   factory TvResponse.fromJson(Map<String, dynamic> json) => TvResponse(
-        tvList: List<TvModel>.from((json['results'] as List)
-            .map((x) => TvModel.fromJson(x))
-            .where((element) => element.posterPath != null)),
-      );
+      tvList: List<TvModel>.from((json["results"] as List)
+          .map((x) => TvModel.fromJson(x))
+          .where((element) => element.posterPath != null)));
 
   @override
   List<Object> get props => [tvList];
--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -2,6 +2,7 @@ import 'package:ditonton/common/network_info.dart';
 import 'package:ditonton/data/datasources/movie_local_data_source.dart';
 import 'package:ditonton/data/datasources/db/database_helper.dart';
 import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
+import 'package:ditonton/data/datasources/tv_remote_data_source.dart';
 import 'package:ditonton/domain/repositories/movie_repository.dart';
 import 'package:ditonton/domain/repositories/tv_repository.dart';
 import 'package:mockito/annotations.dart';
@@ -12,6 +13,7 @@ import 'package:http/http.dart' as http;
   MovieRemoteDataSource,
   MovieLocalDataSource,
   TvRepository,
+  TvRemoteDataSource,
   DatabaseHelper,
   NetworkInfo,
 ], customMocks: [
