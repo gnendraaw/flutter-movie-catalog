d2c10e2208bb67ae135e0ba37c044c28f42283e6
gnendraaw <itsgnendraaw@gmail.com>
2022-12-15 21:50:42 +0800

    created package core

--- core/lib/styles/text_styles.dart
+++ core/lib/styles/text_styles.dart
@@ -1,15 +1,6 @@
+import 'package:core/styles/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
-
-// colors
-const Color kRichBlack = Color(0xFF000814);
-const Color kOxfordBlue = Color(0xFF001D3D);
-const Color kPrussianBlue = Color(0xFF003566);
-const Color kMikadoYellow = Color(0xFFffc300);
-const Color kDavysGrey = Color(0xFF4B5358);
-const Color kGrey = Color(0xFF303030);
 
 // text style
 final TextStyle kHeading5 =
---
+++ core/lib/utils/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
