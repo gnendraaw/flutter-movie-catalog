7a288b6ccd128f8897ce63a962f1c7097e4ee951
gnendraaw <itsgnendraaw@gmail.com>
2022-12-15 23:00:07 +0800

    update home_tv_route

--- core/lib/utils/routes.dart
+++ core/lib/utils/routes.dart
@@ -3,4 +3,4 @@ const POPULAR_MOVIES_ROUTE = '/popular-movie';
 const TOP_RATED_ROUTE = '/top-rated-movie';
 const MOVIE_DETAIL_ROUTE = '/detail';
 const SEARCH_ROUTE = '/search';
-const HOME_TV_ROUTE = 'home-tv';
+const HOME_TV_ROUTE = '/home-tv';
